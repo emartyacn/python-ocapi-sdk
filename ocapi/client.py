784e4a5bbb81dd2e8c027433e3967feb2583800b
Erik Marty <erikomarty@gmail.com>
2020-05-20 19:52:51 -0400

    Some cleanup, deciding on structure

--- ocapi/client.py
+++ ocapi/client.py
@@ -13,6 +13,11 @@ class Auth(Provider):
     TOKEN_URL = 'https://account.demandware.com/dw/oauth2/access_token'
     AUTH_PATH = '/dwsso/oauth2/authorize?client_id=%s&redirect_uri=%s&response_type=%s'
 
+    def __init__(self):
+        self.SITE = 's/-'
+        self.API_TYPE = 'dw/data'
+        self.VER = 'v20_4'
+
     @property
     def creds(self):
         return self.client_id, self.client_secret
@@ -25,15 +30,19 @@ class Auth(Provider):
     def client_secret(self):
         return self.get_credential('client_secret')
 
-    def auth():
-        oauth = OAuth2Session(client_id, redirect_uri=redirect_uri)
-        auth_url, state = oauth.authorization_url(auth_base)
-        #token = oauth.fetch_token(
-        #    token_url,
-        #    client_secret=client_secret,
-        #    include_client_id=True,
-        #    authorization_response=resp,
-        #)
+    @property
+    def hostname(self):
+        return self.get_credential('hostname')
+
+    @property
+    def api_url(self):
+        return 'https://{0}/{1}/{2}/{3}'.format(
+            self.hostname,
+            self.SITE,
+            self.API_TYPE,
+            self.VER
+    )
+
 
     def obtain_token(self):
         provider = Auth()
@@ -45,5 +54,29 @@ class Auth(Provider):
             data=payload,
         ).json()
         token = resp['access_token']
-        print('Authorization Successful')
+        success_msg = """
+        ------------------------
+        Authorization Successful
+        ------------------------
+        """
+        print(success_msg)
         return token
+
+
+    def product_search(self, query):
+        token = self.obtain_token()
+        data = {'data': query}
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer {0}'.format(token)
+        }
+        endpoint = '/product_search?client_id={0}'.format(self.client_id)
+        request_url = '{0}{1}'.format(self.api_url, endpoint)
+        req = requests.post(
+            request_url,
+            headers=headers,
+            data=data,
+            timeout=60,
+        ).json()
+        print('Response:\n'.format(json.dumps(req, indent=2)))
