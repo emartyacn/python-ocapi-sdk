7fa44f76607c325233580f2aefbb5a143d816917
Erik Marty <erikomarty@gmail.com>
2020-05-28 17:44:17 -0400

    Creating a helper function to construct product URL

---
+++ ocapi/lib/helpers.py
@@ -0,0 +1,21 @@
+import json
+import random
+
+import requests
+
+from ocapi.client import ShopAPI
+
+
+def get_product(site_id='-', query=None):
+    shop = ShopAPI()
+    auth = ('storefront', 'PND18')
+    links = [link['link'] for link in shop.product_search(site_id, query)]
+    resp = requests.get(random.choice(links)).json()
+    product_link = 'https://{0}/{1}/{2}/{3}/{4}.html'.format(
+        shop.hostname,
+        resp['c_primaryCategory'],
+        resp['primary_category_id'],
+        resp['name'].replace(' ', '-').lower(),
+        resp['id'])
+    product_url = requests.get(product_link, auth=auth).url
+    return product_url
