0e7f1ef71477954113ffcf6fb83c44638ebc234a
Erik Marty <erikomarty@gmail.com>
2020-05-28 22:45:30 -0400

    Simplifying helper

--- ocapi/lib/helpers.py
+++ ocapi/lib/helpers.py
@@ -1,14 +1,9 @@
-import json
 import random
 
 import requests
 
-from ocapi.client import ShopAPI
 
-
-def get_product(site_id=None, query=None):
-    shop = ShopAPI()
-    links = [link['link'] for link in shop.product_search(site_id, query)]
+def get_product(links):
     resp = requests.get(random.choice(links)).json()
     product_link = 'https://{0}/{1}/{2}/{3}/{4}.html'.format(
         shop.hostname,
