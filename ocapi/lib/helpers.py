38db2196a10068115b076fb15b79ee6edac5fd4d
Erik Marty <erikomarty@gmail.com>
2020-05-28 22:51:35 -0400

    Adding param for hostname

--- ocapi/lib/helpers.py
+++ ocapi/lib/helpers.py
@@ -3,10 +3,10 @@ import random
 import requests
 
 
-def get_product(links):
+def get_product(hostame=None, links):
     resp = requests.get(random.choice(links)).json()
     product_link = 'https://{0}/{1}/{2}/{3}/{4}.html'.format(
-        shop.hostname,
+        hostname,
         resp['c_primaryCategory'],
         resp['primary_category_id'],
         resp['name'].replace(' ', '-').lower(),
