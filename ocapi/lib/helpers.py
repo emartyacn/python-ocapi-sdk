116dadedde8725d8b37b466d287266099a51a663
Erik Marty <erikomarty@gmail.com>
2020-05-29 09:21:35 -0400

    Revert "Updating helper"
    This reverts commit 29fbb5bd879dfd13186fb0ef904e84753b879d92.

--- ocapi/lib/helpers.py
+++ ocapi/lib/helpers.py
@@ -3,9 +3,10 @@ import random
 import requests
 
 
-def get_product(links):
+def get_product(hostname, links):
     resp = requests.get(random.choice(links)).json()
-    product_link = '{0}/{1}/{2}/{3}.html'.format(
+    product_link = 'https://{0}/{1}/{2}/{3}/{4}.html'.format(
+        hostname,
         resp['c_primaryCategory'],
         resp['primary_category_id'],
         resp['name'].replace(' ', '-').lower(),
