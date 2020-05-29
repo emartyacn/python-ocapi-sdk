1c727aa9af3fb734f8818af1deb5bbc934e96405
Erik Marty <erikomarty@gmail.com>
2020-05-28 22:08:45 -0400

    Decalssing

--- ocapi/lib/helpers.py
+++ ocapi/lib/helpers.py
@@ -6,17 +6,14 @@ import requests
 from ocapi.client import ShopAPI
 
 
-class Helpers(ShopAPI):
-
-
-    def get_product(site_id='-', query=None):
-        shop = ShopAPI()
-        links = [link['link'] for link in shop.product_search(site_id, query)]
-        resp = requests.get(random.choice(links)).json()
-        product_link = 'https://{0}/{1}/{2}/{3}/{4}.html'.format(
-            shop.hostname,
-            resp['c_primaryCategory'],
-            resp['primary_category_id'],
-            resp['name'].replace(' ', '-').lower(),
-            resp['id'])
-        return product_link
+def get_product(site_id=None, query=None):
+    shop = ShopAPI()
+    links = [link['link'] for link in shop.product_search(site_id, query)]
+    resp = requests.get(random.choice(links)).json()
+    product_link = 'https://{0}/{1}/{2}/{3}/{4}.html'.format(
+        shop.hostname,
+        resp['c_primaryCategory'],
+        resp['primary_category_id'],
+        resp['name'].replace(' ', '-').lower(),
+        resp['id'])
+    return product_link
